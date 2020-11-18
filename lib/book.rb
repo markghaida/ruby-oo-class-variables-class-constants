947691f7e08b3ffba140d1405627512047a997d4
Mark Ghaida <markghaida@gmail.com>
2020-11-18 07:40:03 -0500

    commit tree a9e538eaf23808c0451bb6417bffce01c87eba64

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,18 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
-
+  GENRES = []
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+ 
   def initialize(title)
-    @title = title
+    @title = title 
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def genre=(genre)
+    @genre = genre 
+    GENRES << genre 
+  end 
 end
